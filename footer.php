e2544566cdc9afbacbef7bbc4b96bfa32bf30d4e
Sarah Duarte <sara8615@stud.kea.dk>
2022-12-06 20:36:47 +0100

    footer-foreløbig

--- footer.php
+++ footer.php
@@ -9,6 +9,89 @@
 
 	</main><!-- #main -->
 
+	<div class= "footer">
+		
+      <nav>
+        <ul>
+          <li>
+            <a href="">DISCOGRAFI</a>
+          </li>
+          <li>
+            <a href="">SOLIST</a>
+          </li>
+          <li>
+            <a href="">ALBUM</a>
+          </li>
+          <li>
+            <a href="">SINGLES</a>
+          </li>
+          <li>
+            <a href="">EP*ER</a>
+          </li>
+		  <li>
+            <a href="">FEATURE</a>
+          </li>
+		  <li>
+            <a href="">BAND</a>
+          </li>
+          <li>
+            <a href="">SINGLER</a>
+          </li>
+          <li>
+            <a href="">ALBUM</a>
+          </li>
+          <li>
+            <a href="">GOSPEL</a>
+          </li>
+          <li>
+            <a href="">PRODUKTIONER</a>
+          </li>
+		  <li>
+            <a href="">BOOKING</a>
+          </li>
+		   <li>
+            <a href="">EVENTS</a>
+          </li>
+		   <li>
+            <a href="">COACHING</a>
+          </li>
+		   <li>
+            <a href="">EN TIME</a>
+          </li>
+		   <li>
+            <a href="">1-1 SPARING</a>
+          </li>
+		   <li>
+            <a href="">KLIPPEKORT</a>
+          </li>
+		   <li>
+            <a href="">KORCOACHING</a>
+          </li>
+		   <li>
+            <a href="">BLOG</a>
+          </li>
+		   <li>
+            <a href="">AGE*ISM</a>
+          </li> 
+		  <li>
+            <a href="">SEX*ISM</a>
+          </li> 
+		  <li>
+            <a href="">BIOGRAFI</a>
+          </li>
+        </ul>
+      </nav>
+	  <div class="footer_socials">
+	<a href="https://www.facebook.com/Misslindaandrews"><img class="facelogo" src=""></a>
+	<a href="https://www.instagram.com/lindaandrewsmusic/"><img class="instalogo" src=""></a>
+	<a href="https://open.spotify.com/artist/3URBm5pWzuvoQ8lWiItKvM"><img class="spotlogo" src=""></a>
+	<a href="https://www.youtube.com/channel/UCFNO91T6Ng-7tH3pdThaM9w"><img class="ytlogo" src=""></a>
+</div>
+<div class="sig_logo"> 
+	<a href="index.php"><img class="LA" src="signatur_linda.svg"></a>
+</div>
+</div>
+
 	<?php do_action( 'ocean_after_main' ); ?>
 
 	<?php do_action( 'ocean_before_footer' ); ?>
