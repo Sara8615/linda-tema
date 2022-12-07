20445d46042c19824256fd0d045cb018110ef31f
Sarah Duarte <sara8615@stud.kea.dk>
2022-12-07 21:04:55 +0100

    pages

---
+++ page-discografy-band.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+get_header(); ?>
+
+<main>
+    <div class="socials">
+	<a href="https://www.facebook.com/Misslindaandrews"><img class="facelogo" src=""></a>
+	<a href="https://www.instagram.com/lindaandrewsmusic/"><img class="instalogo" src=""></a>
+	<a href="https://open.spotify.com/artist/3URBm5pWzuvoQ8lWiItKvM"><img class="spotlogo" src=""></a>
+	<a href="https://www.youtube.com/channel/UCFNO91T6Ng-7tH3pdThaM9w"><img class="ytlogo" src=""></a>
+</div>
+  <article class="disc_grid">
+    <h2 class="album_head">ALBUM</h2>
+
+    <div class="album_liste">
+      <h3>TITEL på album - udgivelses år</h3>
+
+    </div>
+
+    <h2 class="single_head">SINGLER</h2>
+
+    <div class="single_liste">
+      <h3>TITEL på single - udgivelses år</h3>
+      <h3>TITEL på single - udgivelses år</h3>
+      <h3>TITEL på single - udgivelses år</h3>
+      <h3>TITEL på single - udgivelses år</h3>
+
+    </div>
+
+
+  </article>
+</main>
---
+++ page-forside.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+get_header(); ?>
+
+
+<img class="forside_billede" src="" alt="">
+<div class="socials">
+	<a href="https://www.facebook.com/Misslindaandrews"><img class="facelogo" src=""></a>
+	<a href="https://www.instagram.com/lindaandrewsmusic/"><img class="instalogo" src=""></a>
+	<a href="https://open.spotify.com/artist/3URBm5pWzuvoQ8lWiItKvM"><img class="spotlogo" src=""></a>
+	<a href="https://www.youtube.com/channel/UCFNO91T6Ng-7tH3pdThaM9w"><img class="ytlogo" src=""></a>
+</div>
