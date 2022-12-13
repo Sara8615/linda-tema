7dbdd60636dfd1feac07f89987992c36b53900e3
Silas List <silashlist@gmail.com>
2022-12-13 12:44:06 +0100

    boom

--- page-biografy.php
+++ page-biografy.php
@@ -16,10 +16,10 @@ get_header(); ?>
 <img class="bio_billede_1" src="" alt="">
 <div class="socials">
   
-    <a href=https://www.facebook.com/Misslindaandrews><img class="facelogo" src="<?php echo get_stylesheet_directory_uri() ?>/FB.svg" alt="facebook-icon"></a>
-    <a href=https://www.instagram.com/lindaandrewsmusic><img class="instalogo" src="<?php echo get_stylesheet_directory_uri() ?>/insta.svg" alt="insta-icon"></a>
-    <a href=https://open.spotify.com/artist/3URBm5pWzuvoQ8lWiItKvM><img class="spotlogo" src="<?php echo get_stylesheet_directory_uri() ?>/spotify.svg" alt="spotify-icon"></a>
-    <a href=https://www.youtube.com/channel/UCFNO91T6Ng-7tH3pdThaM9w><img class="ytlogo" src="<?php echo get_stylesheet_directory_uri() ?>/youtube.svg" alt="youtube-icon"></a>
+    <a href=https://www.facebook.com/Misslindaandrews><img class="facelogo" src="<?php echo get_stylesheet_directory_uri() ?>/billeder/FB.svg" alt="facebook-icon"></a>
+    <a href=https://www.instagram.com/lindaandrewsmusic><img class="instalogo" src="<?php echo get_stylesheet_directory_uri() ?>/billeder/insta.svg" alt="insta-icon"></a>
+    <a href=https://open.spotify.com/artist/3URBm5pWzuvoQ8lWiItKvM><img class="spotlogo" src="<?php echo get_stylesheet_directory_uri() ?>/billeder/spotify.svg" alt="spotify-icon"></a>
+    <a href=https://www.youtube.com/channel/UCFNO91T6Ng-7tH3pdThaM9w><img class="ytlogo" src="<?php echo get_stylesheet_directory_uri() ?>/billeder/youtube.svg" alt="youtube-icon"></a>
     
   </div>
   <div class="biografi"> 
