1261d22c53b793910674ba56580e769b45b732e1
Sarah Duarte <sara8615@stud.kea.dk>
2022-12-19 11:50:36 +0100

    forside iconer

--- page-forside.php
+++ page-forside.php
@@ -11,16 +11,16 @@
 
 get_header(); ?>
 
-<main>
+<main class="splash">
   <div class="iframecon">
   <iframe width="1200" height="600" src="https://www.youtube.com/embed/WPIq1my_J8o?loop=1&autoplay=1&controls=0&mute=1&playlist=WPIq1my_J8o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	<!-- </div>
-    <img class="forsidebillede" src="<?php echo get_stylesheet_directory_uri() ?>/linda-2.jpg" alt="forsidebillede"> -->
-<div class="socials">
-   <a href=https://www.facebook.com/Misslindaandrews><img class="facelogo" src="<?php echo get_stylesheet_directory_uri() ?>/brun-fb.svg" alt="facebook-icon"></a>
-    <a href=https://www.instagram.com/lindaandrewsmusic><img class="instalogo" src="<?php echo get_stylesheet_directory_uri() ?>/brun-ig.svg" alt="instagram-icon"></a>
-    <a href=https://open.spotify.com/artist/3URBm5pWzuvoQ8lWiItKvM><img class="spotlogo" src="<?php echo get_stylesheet_directory_uri() ?>/brun-spot.svg" alt="spotify-icon"></a>
-    <a href=https://www.youtube.com/channel/UCFNO91T6Ng-7tH3pdThaM9w><img class="ytlogo" src="<?php echo get_stylesheet_directory_uri() ?>/brun-yt.svg" alt="youtube-icon"></a>
+	</div>
+   <!-- <img class="forsidebillede" src="<?php echo get_stylesheet_directory_uri() ?>/linda-2.jpg" alt="forsidebillede"> -->
+<div class="forside-socials">
+   <a href=https://www.facebook.com/Misslindaandrews><img class="facelogo" src="<?php echo get_stylesheet_directory_uri() ?>/orange-fb.svg" alt="facebook-icon"></a>
+    <a href=https://www.instagram.com/lindaandrewsmusic><img class="instalogo" src="<?php echo get_stylesheet_directory_uri() ?>/orange-ig.svg" alt="instagram-icon"></a>
+    <a href=https://open.spotify.com/artist/3URBm5pWzuvoQ8lWiItKvM><img class="spotlogo" src="<?php echo get_stylesheet_directory_uri() ?>/orange-spot.svg" alt="spotify-icon"></a>
+    <a href=https://www.youtube.com/channel/UCFNO91T6Ng-7tH3pdThaM9w><img class="ytlogo" src="<?php echo get_stylesheet_directory_uri() ?>/orange-yt.svg" alt="youtube-icon"></a>
     
   </div>
 
