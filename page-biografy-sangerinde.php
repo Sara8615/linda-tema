4b711ce007cf9e63217af35564a0f05c8b5e2d32
Sarah Duarte <sara8615@stud.kea.dk>
2022-12-19 13:05:50 +0100

    bio siden færdig

--- page-biografy-sangerinde.php
+++ page-biografy-sangerinde.php
@@ -75,8 +75,9 @@ The standard Lorem Ipsum passage, used since the 1500s
  </p>
   <p class="bio_text">  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
  </p>
- <img class="bio_billede_3"src="<?php echo get_stylesheet_directory_uri() ?>/banner-bio.webp" alt="fullwidth-img-2">
+ 
 </div>
+<img class="bio_billede_3"src="<?php echo get_stylesheet_directory_uri() ?>/banner-bio.webp" alt="fullwidth-img-2">
 </main>
 
 <?php
