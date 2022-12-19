63fc46f79b9eb6715fcc70176fa23a0251895039
Silas List <silashlist@gmail.com>
2022-12-19 17:53:07 +0100

    stuff

--- page-booking.php
+++ page-booking.php
@@ -19,63 +19,116 @@ get_header(); ?>
     <section id="popupsparring">
       <div id="luk">&#x2715</div>
       <article class="popuparticlesparring">
+      <img class="popupimg" src="<?php echo get_stylesheet_directory_uri() ?>/1-1.webp" alt="1:1-sparring">
         <h2 class="popuph2">1:1 Sparring</h2>
-        <h3>Pris</h3>
-        <p>wtf</p>
+        <h3>750,-</h3>
+        <p>En times personlig sparring online.</p>
+        <p>Skal du igang med din musikkarriere eller er du gået i stå med musikken, eller bare at sparre med med én som har levet af musikken i 20 år?</p>
+        <p>Jeg vil give dig alle de allerbedste råd og tips til hvordan du kan komme videre i din karriere og give dig konkrete råd. Hvis du har noget musik indspillet eller optaget lytter jeg det igennem + læser dit pressemateriale for også at kunne give feedback på dette. Du får sangcoachingen optaget så du kan gå tilbage og bruge det igen og igen. Vi aftaler sammen en dag og tidspunkt der passer dig.. Oplagt som gave til en kunstner/sanger/musiker/skuespiller. Der kan undervises både på dansk, færøsk, norsk eller engelsk.</p>
+        <h3>Tilbud!!</h3>
+        <p>5 turs klippekort á 1 times online sparring 3.000,- (spar 250,-)</p>
+        <p>10 turs klippekort á 1 times online sparring 7.000,- (spar 500,-)</p>
       </article>
     </section>
 
       <section id="popupsangcoaching">
       <div id="luk2">&#x2715</div>
       <article class="popuparticlesparring">
+        <img class="popupimg" src="<?php echo get_stylesheet_directory_uri() ?>/coaching.webp" alt="Sangcoaching">
         <h2 class="popuph2">Sangcoaching</h2>
-        <h3>Pris</h3>
-        <p>wtf</p>
+        <h3>750,-</h3>
+        <p>En times personlig sang og stemmecoaching.</p>
+        <p>Du bestemmer hvad vi skal arbejde med og hvad du ønsker hjælp til.</p>
+        <p>Hvis du har noget musik indspillet eller optaget, lytter jeg det igennem igennem og vil give dig konkrete tips til hvordan du kan nå dine mål.</p>
+        <p>Du får sangcoachingen optaget så du kan gå tilbage og bruge det igen og igen.</p>
+        <p>Vi aftaler sammen en dag og tidspunkt der passer dig.</p>
+        <p>Oplagt som gave til en kunstner/sanger/musiker/skuespiller/speaker/lærer.</p>
+        <p>Der kan undervises både på dansk, færøsk, norsk eller engelsk.</p>
+        <h3>Tilbud!!</h3>
+        <p>5 turs klippekort á 1 times online sangcoaching til 3.500,- (spar 250,-)</p>
+        <p>10 turs klippekort á 1 times online sangcoaching til 7.000,- (spar 500,-)</p>
       </article>
     </section>
 
       <section id="popupklippekort">
       <div id="luk3">&#x2715</div>
       <article class="popuparticlesparring">
+<img class="popupimg" src="<?php echo get_stylesheet_directory_uri() ?>/kor.webp" alt="Klippekort">
+
         <h2 class="popuph2">Klippekort</h2>
-        <h3>Pris</h3>
-        <p>wtf</p>
+        <h3>?</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </article>
     </section>
 
       <section id="popupkorcoaching">
       <div id="luk4">&#x2715</div>
       <article class="popuparticlesparring">
+<img class="popupimg" src="<?php echo get_stylesheet_directory_uri() ?>/kor-coach.webp" alt="Kor Coaching">
         <h2 class="popuph2">Korcoaching</h2>
-        <h3>Pris</h3>
-        <p>wtf</p>
+        <h3>1000,-</h3>
+        
+        <p>En times undervisning til hele dit kor.</p>
+        <p>Jeg vil indstuderet en aftalt sang og give gode tips til hvordan man bliver en bedre korsanger og hvordan koret som helhed kan udvikle sig.</p>
+        <p>Jeg vil komme ind på sangteknik og de forskellige klange i stemmegrupperne.</p>
+        <p>Der vil blive mulighed for at stille spørgsmål enten inden eller undervejs.</p>
+        <p>Undervisningen vil blive optaget så koret kan gå tilbage og bruge det igen og igen.</p>
+        <p>Undervisningen kan foregå på Zoom eller den platform som ønskes.</p>
+        <p>Vi aftaler sammen en dag og tidspunkt der passer jer.</p>
+        <p>Oplagt som gave/ekstra undervisning til sit kor.</p>
+        <p>Der kan undervises både på dansk, færøsk, norsk eller engelsk.</p>
+        <h3>Tilbud!!</h3>
+        <p>Hvis i ønsker flere gange kan jeg lave et godt tilbud til Jer!</p>
       </article>
     </section>
 
       <section id="popupconcert">
       <div id="luk5">&#x2715</div>
       <article class="popuparticlesparring">
-        <h2 class="popuph2">Concert</h2>
-        <h3>Pris</h3>
-        <p>wtf</p>
+<img class="popupimg" src="<?php echo get_stylesheet_directory_uri() ?>/koncert.webp" alt="concert">
+
+        <h2 class="popuph2"> Online koncert</h2>
+        <h3>Spørg for pris</h3>
+        <p>En times online koncert med mig og mit band.</p>
+        <p>Evt med mulighed for at stille spørgsmål enten inden eller undervejs.</p>
+        <p>Mulighed for at koncerten bliver optaget så i kan lægge den på en intern platform.</p>
+        <p>Koncerten kan foregå på Zoom eller på den platform som ønskes.</p>
+        <p>Obs……Skriv for pris!!!</p>
+        <p>Vi aftaler sammen en dag og tidspunkt der passer jer.</p>
+        <p>Oplagt som særlig gave til jeres medarbejdere eller forening.</p>
+
       </article>
     </section>
 
       <section id="popupforedrag">
       <div id="luk6">&#x2715</div>
       <article class="popuparticlesparring">
+<img class="popupimg" src="<?php echo get_stylesheet_directory_uri() ?>/fordrag.webp" alt="foredrag">
         <h2 class="popuph2">Foredrag</h2>
-        <h3>Pris</h3>
-        <p>wtf</p>
+        <h3>Spørg for pris</h3>
+        <p>En times online foredrag om mit musikalske erhverv.</p>
+        <p>Helt fra min start om at synge i kirken i Thorshavn til X-Factor til hvor jeg er idag.</p>
+        <p>Evt med mulighed for at stille spørgsmål enten inden eller undervejs.</p>
+        <p>Jeg giver selvfølgelig en sang eller to.</p>
+        <p>Mulighed for at foredraget bliver optaget så i kan lægge den på en intern platform</p>
+        <p>Foredraget kan foregå på Zoom eller på den platform som ønskes.</p>
+        <p>Obs……Skriv for pris!!!</p>
+        <p>Vi aftaler sammen en dag og tidspunkt der passer jer.</p>
+        <p>Oplagt som særlig gave til jeres medarbejdere eller forening.</p>
       </article>
     </section>
 
       <section id="popuppersonlighilsen">
       <div id="luk7">&#x2715</div>
       <article class="popuparticlesparring">
+<img class="popupimg" src="<?php echo get_stylesheet_directory_uri() ?>/hilsen.webp" alt="personlig hilsen">
+
         <h2 class="popuph2">Personlig Hilsen</h2>
-        <h3>Pris</h3>
-        <p>wtf</p>
+        <h3>Spørg for pris</h3>
+        <p>Vi står midt i en Corona pandemi hvor flere og flere bliver ensomme og savner et kram og smil, og det her er min måde at prøve at hjælpe til med at få sangen ind i hjemmene og på arbejdspladsen i håb om at folk bliver glade og føler sig værdsat. OBS….Prisen er et særligt og tidsbegrænset tilbud i denne svære tid, så skynd dig og book din hilsen nu.
+        Vi aftaler sammen hvilken sang + hilsen der være i videoen.</p>
+      <p>Jeg sørger for at det bliver personligt og autentisk, og ikke mindst smukt.
+         Sange som Amazing Grace, Lean on me, You’ve got a friend, Stand by me, Hallelujah osv. kan være med til at gøre dagen helt særlig; og husk en personlig optagelse med en sanghilsen holder meget længere en blomster. Oplagt som særlig gave til et familiemedlem eller én man ønsker at glæde med sin sang. </p>
       </article>
     </section>
    
@@ -136,7 +189,7 @@ og stemme coaching...</p>
 
 <div id="optionconcert">
 <img class="option-img" src="<?php echo get_stylesheet_directory_uri() ?>/koncert.webp" alt="concert">
-<h3 class="titel">CONCERT </h3>
+<h3 class="titel">KONCERT </h3>
 <h3 class="pris"> Spørg for pris </h3>
 <p class="beskrivelse"> En times concert med mig og mit band... </p>
 </div>
