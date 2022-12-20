74f9b82487da3fa795167d34050e800f5793508a
Sarah Duarte <sara8615@stud.kea.dk>
2022-12-20 11:54:24 +0100

    noget med billeder

--- page-blog-linda-andrews.php
+++ page-blog-linda-andrews.php
@@ -27,8 +27,9 @@ get_header(); ?>
     
     <p class="element_2"> de Finibus Bonorum et Malorum, written by Cicero in 45 BC Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
 </p>
+<div class="element_3_img">
 <img class="element_3" src="<?php echo get_stylesheet_directory_uri() ?>/top-banner-blog.webp" alt="billede">
-
+</div>
 
     </article>
 
