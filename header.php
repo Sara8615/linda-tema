b8a0b364bef31443ff396d4f8a57688cc823e961
Silas List <silashlist@gmail.com>
2022-12-20 09:42:59 +0100

    burger

--- header.php
+++ header.php
@@ -32,15 +32,26 @@
 
 </head>
 
-<body 
+<body>
 
  <header>
-      
-      <nav class="navbar">
-		<div class="logomobilmenu">           
-				 <a href=https://sarahduarte.dk/kea/10_eksamen/forside><img class="linda-logo" src="<?php echo get_stylesheet_directory_uri() ?>/orange-linda-logo.svg" alt="linda-logo"></a>
-		</div>
+    <div class="burger">
+<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+	<div class="burgergrid">		
+	<a class="logocontainer" href=https://sarahduarte.dk/kea/10_eksamen/forside><img class="linda-logo-burger" src="<?php echo get_stylesheet_directory_uri() ?>/orange-linda-logo.svg" alt="linda-logo"></a>
 
+		<div id="logomobilmenu" class="logoburgermenu">  
+				<a  class="burgerpunkt" href="http://www.listeportfolio.dk/kea/2-sem-eksamen/lindaandrews/discografy/">DISKOGRAFI</a>
+				<a  class="burgerpunkt" href="https://sarahduarte.dk/kea/10_eksamen/blog/">BLOG</a>
+				<a  class="burgerpunkt" href="https://sarahduarte.dk/kea/10_eksamen/biografy/">BIOGRAFI</a>
+ 				<a  class="burgerpunkt" href="https://sarahduarte.dk/kea/10_eksamen/booking/">BOOKING</a>
+  				<a  class="burgerpunkt" href="https://sarahduarte.dk/kea/10_eksamen/events/">EVENTS</a>
+    			<a class="burgerpunkt"  href="https://sarahduarte.dk/kea/10_eksamen/coaching/">COACHING</a>
+		</div>
+</div>
+</div>
+  		<nav class="navbar">
         <ul class="hovedmenu nav-menu">
 			
           <li class="limenu">
@@ -68,8 +79,43 @@
         </ul>
 	
       </nav>
-	  
+	  	<script>
+ 	// document.querySelector("#openclose").addEventListener("click", () => (logomobilmenu.style.display = "block"));
+
+    // document.querySelector("#openclose").addEventListener("click", () => (logomobilmenu.style.display = "none"));
+
+ 	// document.querySelector("#openclose").addEventListener("click", () => (logomobilmenu.style.display = "block"));
+
+	// document.querySelector("#openclose").addEventListener("click", click);
+	
+	// function click(){
+	// document.querySelector("#logomobilmenu").classList.remove("active");
+	// document.querySelector("#logomobilmenu").classList.add("active");
+	// }
+
+	function myFunction() {
+  var x = document.getElementById("logomobilmenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// const burger = document.querySelector(".burger");
+// const menu = document.querySelector(".nav-menu");
+
+// burger.addEventListener("click", () => {
+//   burger.classList.toggle("active");
+//   menu.classList.toggle("active");
+// });
+
+
+
+	</script>
     </header>
+
+
 
 
 
